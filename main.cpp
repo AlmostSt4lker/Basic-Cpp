6398a9af770831ed42d65b9d040519075bd1eaa6
AlmostSt4lker <adamx851@gmail.com>
2017-06-05 17:57:10 +0200

    Fibonacci Numbers

--- main.cpp
+++ main.cpp
@@ -1,69 +1,29 @@
 #include <iostream>
-#include <stdio.h>
-#include <windows.h>
-#include <conio.h>
+#include <iomanip>
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-float x,y;
-char wybor;
-int main(int argc, char** argv) {
-	while (true)
+
+long double fib[100000];
+int n,i;
+
+int main(int argc, char** argv) 
+{
+	cout<<"Ile liczb fibonacziego mam wyznaczyc: ";
+	cin>>n;
+	
+	fib[0]=1;
+	fib[1]=1;
+	
+	for (i=2;i<n;i++)
 	{
-	
-		cout<<"Podaj 1 liczbe: ";
-		cin>>x;
-		cout<<"Podaj 2 liczbe: ";
-		cin>>y;
-	cout<<endl;
-	cout<<"-MENU-"<<endl;
-	cout<<"==================="<<endl;
-	cout<<"1. Dodawanie"<<endl;
-	cout<<"2. Odejmowanie"<<endl;
-	cout<<"3. Mnozenie"<<endl;
-	cout<<"4. Dzielenie"<<endl;
-	cout<<"5. Zamknij Program"<<endl;
-
-	wybor=getch();
-		switch(wybor)
-		{
-			case '1':
-				{
-					cout<<"Wynik dodawania to : ";
-					cout<<x+y<<endl;
-				}
-				
-			break;
-			case '2':
-			{
-					cout<<"Wynik odejmowania to : ";
-					cout<<x-y<<endl;
-				}
-				
-			break;
-			case '3':
-				{
-					cout<<"Wynik mnozenia to : ";
-					cout<<x*y<<endl;
-				}
-			break;
-			case '4':
-				{
-					if (y==0) cout<<"Nie dzielimy przez 0"<<endl;
-					else cout<<"Wynik dzielenia to : "<<x/y<<endl;
-					
-				}
-			break;
-			case '5':
-				{
-					exit(0);	
-				}
-			
-			default: cout<<"Nie ma takiej opcji w menu !"<<endl;
-	
-		}
-			getchar();
+		fib[i]= fib[i-1] + fib[i-2];	
 	}
-
-
+	cout<<setprecision(10000);
+		for (i=0;i<n;i++)
+	{
+		cout<<endl<<"wyraz nr"<<i+1<<" : "<<fib[i]<<endl;	
+	}
+	cout<<endl<<"Wyraz nr "<<n<<": "<<fib[n-1]<<endl;
+	system("PAUSE");
 	return 0;
 }
